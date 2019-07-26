cd677d7fc4a7c6fe51d516a96efaf961576c9be7
Laura Radville <laura.radville@gmail.com>
2019-07-26 00:38:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,6 +7,12 @@ class CashRegister
     @items = []
   end
   
-  
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    # self.last_transaction = amount * quantity
+  end
   
 end
